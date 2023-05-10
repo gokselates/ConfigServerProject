5fa95b7f9bbe2f234ef94c67322d3d842866707e
gokselates <gokselates@gmail.com>
2023-05-10 11:32:41 +0300

    Initial commit

---
+++ src/main/java/com/configserverproject/ConfigServerProject/ConfigServerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.configserverproject.ConfigServerProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerProjectApplication.class, args);
+	}
+
+}
