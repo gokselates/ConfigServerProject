41ec3e779e775956776a4e6ee1eea565f7ed2e2d
gokselates <gokselates@gmail.com>
2023-05-10 11:48:01 +0300

    Initial commit

--- src/main/java/com/configserverproject/ConfigServerProject/ConfigServerProjectApplication.java
+++ src/main/java/com/configserverproject/ConfigServerProject/ConfigServerProjectApplication.java
@@ -2,8 +2,10 @@ package com.configserverproject.ConfigServerProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerProjectApplication {
 
 	public static void main(String[] args) {
